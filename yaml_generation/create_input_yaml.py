491ccef9897ad8aabfe6f6e2d8c2b3914d63017d
Ryan Morelock <rymo1354@colorado.edu>
2020-01-13 10:01:45 -0700

    Ordered arguments for create_input_yaml.py

--- yaml_generation/create_input_yaml.py
+++ yaml_generation/create_input_yaml.py
@@ -14,40 +14,66 @@ def argument_parser():
         '-c', '--copyfile_name', help='Copied .yml file path', type=str, default=None)
     parser.add_argument(
         '-e', '--edit_fields', help='Fields to edit', nargs='+', type=str,
-        choices=['INCAR_Tags', 'Relaxation_Set', 'MPIDs', 'PATHs',
-                 'Magnetization_Scheme', 'Calculation_Type',
+        choices=['MPIDs', 'PATHs', 'Calculation_Type', 'Relaxation_Set',
+                 'Magnetization_Scheme', 'INCAR_Tags', 'KPOINTs',
                  'Max_Submissions'], default=None)
     args = parser.parse_args()
 
     return args
 
-def yml_inputs(args):
+def reorder_args(args):
+    input_args_ind = []
+    ordering = ['MPIDs', 'PATHs', 'Calculation_Type', 'Relaxation_Set',
+                'Magnetization_Scheme', 'INCAR_Tags', 'KPOINTs',
+                'Max_Submissions']
+    try:
+        for arg in args.edit_fields:
+            if arg in ordering:
+                input_args_ind.append(ordering.index(arg))
+            else:
+                print('Bad argument passed; fatal error')
+                sys.exit(1)
+        sorted_inputs_ind = sorted(input_args_ind)
+        sorted_inputs = [ordering[val] for val in sorted_inputs_ind]
+        return sorted_inputs
+    except TypeError:
+        return None
+
+def yml_inputs(args, args_fields):
+    # add in something to organize the fields read-in in the manner that you want them read-in
     WY = WriteYaml(args.copyfile_name)
 
-    for field in args.edit_fields:
+    if args_fields is None:
+        pass
 
-        if field == 'MPIDs':
-            WY.validate_mpids()
+    else:
+        for field in args_fields:
 
-        if field == 'PATHs':
-            WY.validate_paths()
+            if field == 'MPIDs':
+                WY.validate_mpids()
 
-        if field == 'Calculation_Type':
-            WY.validate_calculation_type()
+            if field == 'PATHs':
+                WY.validate_paths()
 
-        if field == 'Relaxation_Set':
-            WY.validate_general_string('Relaxation_Set', WY.allowed_relaxation_sets,
-                                       'Base Relax Set from Materials Project (')
+            if field == 'Calculation_Type':
+                WY.validate_calculation_type()
 
-        if field == 'Magnetization_Scheme':
-            WY.validate_magnetization()
+            if field == 'Relaxation_Set':
+                WY.validate_general_string('Relaxation_Set', WY.allowed_relaxation_sets,
+                                           'Base Relax Set from Materials Project (')
 
-        if field == 'INCAR_Tags':
-            WY.add_or_edit_convergence_step()
+            if field == 'Magnetization_Scheme':
+                WY.validate_magnetization()
 
-        if field == 'Max_Submissions':
-            WY.validate_general_positive_integer('Max_Submissions',
-                                                 'Number of Workflow Submissions')
+            if field == 'INCAR_Tags':
+                WY.add_or_edit_convergence_step()
+
+            if field == 'KPOINTs':
+                WY.add_or_edit_kpoints()
+
+            if field == 'Max_Submissions':
+                WY.validate_general_positive_integer('Max_Submissions',
+                                                     'Number of Workflow Submissions')
 
     return WY.new_dictionary
 
@@ -65,7 +91,8 @@ def write_yaml(write_dict, path):
 
 def main():
     args = argument_parser()
-    yml_dict = yml_inputs(args)
+    ordered_args = reorder_args(args)
+    yml_dict = yml_inputs(args, ordered_args)
     write_yaml(yml_dict, args.outfile_name)
 
 if __name__ == "__main__":
